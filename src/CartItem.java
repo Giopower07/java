c44d8f1e4848340abdfc665449b000ae34fb0918
Giopower07 <giorgioantonio.palermo@branchina.edu.it>
2025-05-28 10:11:56 +0200

    public CartItem . Questo è un metodo costruttore di tipo pubblico nel quale passiamo due parametri

--- src/CartItem.java
+++ src/CartItem.java
@@ -7,6 +7,12 @@ public class CartItem {
     private Product product;
     private int quantity;
 
+    /** Metodo costruttore nel quale si usa il this,
+     * qui troviamo due parametri ,uno di tipo int e uno di tipo Product
+     *
+     * @param product
+     * @param quantity
+     */
     public CartItem(Product product, int quantity) {
         this.product = product;
         this.quantity = quantity;
